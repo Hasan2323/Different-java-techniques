4b2bc910e0949cdc8f015b68bc116177f019606d
Saimon <ahsaimon.ctg@gmail.com>
2020-06-13 00:12:12 +0600

    discussed System.out.format/System.out.printf() functionality

---
+++ src/FormattedStrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// https://www.homeandlearn.co.uk/java/java_formatted_strings.html details here
+
+public class FormattedStrings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 1; i++) {
+            String s1=sc.next();
+            int x=sc.nextInt();
+
+            System.out.printf("%-15s %03d %n", s1, x);
+            //System.out.format("%-15s %03d %n", s1, x);
+        }
+
+
+    }
+}
