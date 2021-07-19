139072e597efb693312ca7390e091bea15ddcca0
Saimon <ahsaimon.ctg@gmail.com>
2021-07-20 02:47:20 +0600

    Question one

---
+++ src/QuestionOne.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Muhammad Saimon
+ * @since 15/07/21 11:57 PM
+ */
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        int x = 10;
+/*        if (x) // required Boolean. here int cannot be converted to boolean
+            System.out.println("Hello");
+        else
+            System.out.println("Bye");*/
+    }
+}
